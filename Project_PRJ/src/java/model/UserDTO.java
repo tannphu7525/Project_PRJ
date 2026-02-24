b613d46cb1adb209e5e931b3401295788413aca8
tannphu7525 <thaiphu222@gmail.com>
2026-02-24 12:14:05 +0700

    getter

--- Project_PRJ/src/java/model/UserDTO.java
+++ Project_PRJ/src/java/model/UserDTO.java
@@ -9,5 +9,28 @@ package model;
  * @author Cuong
  */
 public class UserDTO {
+    private String username, password;
+
+    public UserDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
     
 }
