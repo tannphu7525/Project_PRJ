691a27173f95ebc84ca2f16d13693a07bdec31a4
TanPhu <thaiphu222@gmail.com>
2026-02-25 23:48:10 +0700

    Done Dang Ki Thanh Vien

--- Project_PRJ/src/java/controller/RegisterController.java
+++ Project_PRJ/src/java/controller/RegisterController.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
 import java.io.IOException;
@@ -12,25 +11,68 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.UserDAO;
+import model.UserDTO;
 
-@WebServlet(name="RegisterController", urlPatterns={"/RegisterController"})
+@WebServlet(name = "RegisterController", urlPatterns = {"/RegisterController"})
 public class RegisterController extends HttpServlet {
-   
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
 
-    } 
+    private static final String ERROR_PAGE = "register.jsp";
+    private static final String SUCCESS_PAGE = "login.jsp";
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+        String username = request.getParameter("username");
+        String fullName = request.getParameter("fullName");
+        String password = request.getParameter("password");
+        String confirmpassword = request.getParameter("confirmPassword");
+
+        String url = ERROR_PAGE;
+        String msg = "";
+
+        try {
+            if (!password.equals(confirmpassword)) {
+                msg = "Mật khẩu không trùng khớp!";
+                request.setAttribute("msg", msg);
+            } else {
+                UserDAO dao = new UserDAO();
+                if (dao.checkDuplicateUsername(username)) {
+                    msg = "Username này đã được sử dụng. Vui lòng chọn Username khác";
+                    request.setAttribute("msg", msg);
+                } else {
+                    UserDTO newUser = new UserDTO(0, username, password, fullName, "CUSTOMER", true);
+                    boolean isSuccess = dao.registerUser(newUser);
+
+                    if (isSuccess) {
+                        msg = "Đăng ký thành công! Vui lòng đăng nhập.";
+                        request.setAttribute("msg", msg);
+                        url = SUCCESS_PAGE;
+                    } else {
+                        msg = "Có lỗi xảy ra trong quá trình đăng ký. Vui lòng thử lại.";
+                        request.setAttribute("msg", msg);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            request.getRequestDispatcher(url).forward(request, response);
+        }
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
--- Project_PRJ/src/java/model/UserDAO.java
+++ Project_PRJ/src/java/model/UserDAO.java
@@ -9,6 +9,18 @@ public class UserDAO {
 
     private static final String LOGIN_QUERY = "SELECT * FROM Users WHERE Username = ? AND Password = ?";
     private static final String CHECK_DUPLICATE_USERNAME_QUERY = "SELECT Username FROM Users WHERE Username = ?";
+    private static final String REGISTER_QUERY = "INSERT INTO [dbo].[Users]\n" +
+                                                "           ([Username]\n" +
+                                                "           ,[Password]\n" +
+                                                "           ,[FullName]\n" +
+                                                "           ,[Role]\n" +
+                                                "           ,[Status])\n" +
+                                                "     VALUES\n" +
+                                                "           (?\n" +
+                                                "           ,?\n" +
+                                                "           ,?\n" +
+                                                "           ,?\n" +
+                                                "           ,?)";
     
     // Login DAO
     public UserDTO login(String txtUsername, String txtPassword) {
@@ -89,7 +101,31 @@ public class UserDAO {
     }
     
     // Register DAO
-//    public UserDTO registerUser(){
-//        
-//    }
+    public boolean registerUser(UserDTO user){
+        boolean check = false;
+        Connection conn = null;
+        PreparedStatement stm = null;
+
+        try {
+            conn = DBUtils.getConnection();
+            if (conn != null) {
+                stm = conn.prepareStatement(REGISTER_QUERY);
+                stm.setString(1, user.getUsername());
+                stm.setString(2, user.getPassword());
+                stm.setString(3, user.getFullName());
+
+                check = stm.executeUpdate() > 0;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null) conn.close();
+                if (stm != null) stm.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return check;             
+    }
 }
