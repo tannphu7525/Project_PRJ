fecb770c1270628ad4447f489f4e1a1f51e9d383
TanPhu <thaiphu222@gmail.com>
2026-02-26 23:29:07 +0700

    Update Code HomeController

--- Project_PRJ/src/java/model/MovieDAO.java
+++ Project_PRJ/src/java/model/MovieDAO.java
@@ -4,10 +4,91 @@
  */
 package model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import util.DBUtils;
+
 /**
  *
  * @author admin
  */
 public class MovieDAO {
+    //CRUD
+
+    private final static String GET_ALL_MOVIE = "SELECT * FROM Movies";
+    private final static String GET_ACTIVE_MOVIE = "SELECT * FROM Movies WHERE Status = 1";
+    private final static String INSERT_MOVIE = "INSERT INTO [dbo].[Movies]\n" +
+                                                "           ([Title]\n" +
+                                                "           ,[Description]\n" +
+                                                "           ,[PosterUrl]\n" +
+                                                "           ,[Genre]\n" +
+                                                "           ,[BasePrice]\n" +
+                                                "           ,[Status])\n" +
+                                                "     VALUES\n" +
+                                                "           (?\n" +
+                                                "           ,?\n" +
+                                                "           ,?\n" +
+                                                "           ,?\n" +
+                                                "           ,?\n" +
+                                                "           ,?";
+    private final static String DELETE_MOVIE = "UPDATE [dbo].[Movies] SET [Status] = 0 WHERE MovieID = ?";
+    //Lấy ra tất cả Movie
+    public ArrayList<MovieDTO> getAllMovie() {
+        ArrayList<MovieDTO> list = new ArrayList<>();
+        try ( Connection conn = DBUtils.getConnection();
+                PreparedStatement stm = conn.prepareStatement(GET_ALL_MOVIE)) {
+            try ( ResultSet rs = stm.executeQuery()) {
+                while (rs.next()) {
+                    int movieID = rs.getInt("movieID");
+                    String title = rs.getString("title");
+                    String description = rs.getString("description");
+                    String posterUrl = rs.getString("posterUrl");
+                    String genre = rs.getString("genre");
+                    double basePrice = rs.getDouble("basePrice");
+                    boolean status = rs.getBoolean("status");
+
+                    MovieDTO movie = new MovieDTO(movieID, title, description, posterUrl, genre, basePrice, status);
+                    list.add(movie);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
     
+    //INSERT New Movie
+    public boolean insertMovie(MovieDTO movie) {
+        boolean check = false;
+        try ( Connection conn = DBUtils.getConnection();
+                PreparedStatement stm = conn.prepareStatement(GET_ALL_MOVIE)) {
+        stm.setString(1, movie.getTitle());
+        stm.setString(2, movie.getDescription());
+        stm.setString(3, movie.getPosterUrl());
+        stm.setString(4, movie.getGenre());
+        stm.setDouble(5, movie.getBasePrice());
+        stm.setBoolean(6, movie.isStatus());
+        
+        check = stm.executeUpdate() > 0; 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return check;
+    }    
+    
+    //DELETE MOVIE
+    public boolean deleteMovie(int movieID) {
+        boolean check = false;
+        try ( Connection conn = DBUtils.getConnection();
+                PreparedStatement stm = conn.prepareStatement(DELETE_MOVIE)) {
+        stm.setInt(1,movieID);      
+        check = stm.executeUpdate() > 0; 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return check;
+    }      
 }
--- Project_PRJ/src/java/model/MovieDTO.java
+++ Project_PRJ/src/java/model/MovieDTO.java
@@ -9,5 +9,78 @@ package model;
  * @author admin
  */
 public class MovieDTO {
-    
+    private int movieID;
+    private String title;
+    private String description;
+    private String posterUrl;
+    private String genre;
+    private double basePrice;
+    private boolean status;
+
+    public MovieDTO(int movieID, String title, String description, String posterUrl, String genre, double basePrice, boolean status) {
+        this.movieID = movieID;
+        this.title = title;
+        this.description = description;
+        this.posterUrl = posterUrl;
+        this.genre = genre;
+        this.basePrice = basePrice;
+        this.status = status;
+    }
+
+    public int getMovieID() {
+        return movieID;
+    }
+
+    public void setMovieID(int movieID) {
+        this.movieID = movieID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    public void setPosterUrl(String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(double basePrice) {
+        this.basePrice = basePrice;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
 }
